29fef0d25f0f9d0225df24908798efe3f059afaa
Ricardo <ricardocarvalhosilva786@gmail.com>
2023-09-22 15:25:02 -0300

    atualização 22/09/23

--- Ternario.java
+++ Ternario.java
@@ -6,27 +6,27 @@ public class Ternario {
 
         String resultado = a==b ?"Verdadeiro" : "Falso";
 
-            System.out.println(resultado);
+            System.out.println("A é igual a B? " + resultado);
 
         String resultado1 = a>=b ?"Verdadeiro" : "Falso";
         
-            System.out.println(resultado1);
+            System.out.println("A é igual ou maior que B? " + resultado1);
 
         String resultado2 = a<=b ?"Verdadeiro" : "Falso";
 
-            System.out.println(resultado2);
+            System.out.println("A é menor ou igual a B? " + resultado2);
 
         String resultado3 = a!=b ?"Verdadeiro" : "Falso";
 
-            System.out.println(resultado3);
+            System.out.println("A é diferente de B? " + resultado3);
 
         String resultado4 = a>b ?"Verdadeiro" : "Falso";
         
-            System.out.println(resultado4);
+            System.out.println("A é maior que B? " + resultado4);
 
         String resultado5 = a<b ?"Verdadeiro" : "Falso";
 
-            System.out.println(resultado5);
+            System.out.println("A é menor que B? " + resultado5);
      }
     
 }
