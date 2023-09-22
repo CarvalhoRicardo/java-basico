75ae4e36903f33e75577f303e3214ecaa2a13f69
Ricardo <ricardocarvalhosilva786@gmail.com>
2023-09-22 15:07:46 -0300

    novo material

---
+++ Ternario.java
@@ -0,0 +1,20 @@
+public class Ternario {
+    public static void main(String[] args) {
+
+    int a = 6;
+    int b = 7;
+
+        String resultado = a==b ?"Verdadeiro" : "Falso";
+
+            System.out.println(resultado);
+
+        String resultado1 = a>=b ?"Verdadeiro" : "Falso";
+        
+            System.out.println(resultado1);
+
+        String resultado2 = a<=b ?"Verdadeiro" : "Falso";
+
+            System.out.println(resultado2);
+    }
+    
+}
