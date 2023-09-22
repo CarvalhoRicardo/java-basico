4aae2002e1e2a395cd556df746e40d437741ee7f
Ricardo <ricardocarvalhosilva786@gmail.com>
2023-09-22 10:08:25 -0300

    Testando operadores

---
+++ Operadores.java
@@ -0,0 +1,37 @@
+public class Operadores {
+    public static void main(String[] args) {
+
+    int soma = 1 + 1;
+
+        System.out.println(soma);
+
+    int subtracao = 5 - 4;
+
+        System.out.println(subtracao);
+
+    int multiplicacao = 5 * 5;
+
+        System.out.println(multiplicacao);
+
+    int divisao = 10 / 2;
+
+        System.out.println(divisao);
+
+    double multiplicacaoFracao = 6.4 * 5.5;
+
+        System.out.println(multiplicacaoFracao);
+
+    double subtracaoFracao = 2.5 - 5.5;
+
+        System.out.println(subtracaoFracao);
+
+    double somaFracao = 2.5 + 1.5;
+
+        System.out.println(somaFracao);
+
+    String nomeCompleto = "LINGUAGEM" + "JAVA";
+    
+        System.out.println(nomeCompleto);
+
+    }
+}
